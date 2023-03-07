adba6be5245977cdd4e209482b034042dd1326dd
LTF <17839328897@163.com>
2023-03-07 14:59:18 +0800

    first commit

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello{
+    public static void main (String[] args){
+        int age=18
+        double score =80.4
+        char gender = '男'  // 字符
+        String name = "jack" // 字符串
+
+        System.out.println("hello,world");
+    }
+}
